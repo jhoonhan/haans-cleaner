cb6e0f3f967ab22e85d06ca2f37909dcf1d28152
jhoonhan <jhoon.han@outlook.com>
2022-02-27 21:50:37 -0500

    2/27

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -31,6 +31,29 @@ import {
   D_SET_GEOCODE,
 } from "./types";
 
+// export const fetchGeocode = (address) => async (dispatch) => {
+//   console.log(address);
+//   const { street, city, zip } = address;
+//   const queryStreet = street
+//     .replace(/[^a-zA-Z0-9 ]/g, "")
+//     .split(" ")
+//     .join("+");
+//   const queryCity = city
+//     .replace(/[^a-zA-Z0-9 ]/g, "")
+//     .split(" ")
+//     .join("+");
+//   const queryZip = zip;
+
+//   const res = await GoogleGeocode.get(
+//     `/geocode/json?address=${queryStreet},+${queryCity},+${queryZip}&key=${process.env.REACT_APP_GOOGLE_GEOCODING}`
+//   );
+//   const coords = res.data.results[0].geometry.location;
+//   // dispatch({ type: D_GET_COORDS, payload: coords });
+
+//   return coords;
+// };
+//
+
 //////////////// USER
 export const signIn = ({ isSignedIn, userProfile }) => {
   return {
@@ -185,29 +208,6 @@ export const compeleteOrder = (orderId, data) => async (dispatch) => {
     payload: { ...res1.data, acceptId: data.id },
   });
 };
-
-// export const fetchGeocode = (address) => async (dispatch) => {
-//   console.log(address);
-//   const { street, city, zip } = address;
-//   const queryStreet = street
-//     .replace(/[^a-zA-Z0-9 ]/g, "")
-//     .split(" ")
-//     .join("+");
-//   const queryCity = city
-//     .replace(/[^a-zA-Z0-9 ]/g, "")
-//     .split(" ")
-//     .join("+");
-//   const queryZip = zip;
-
-//   const res = await GoogleGeocode.get(
-//     `/geocode/json?address=${queryStreet},+${queryCity},+${queryZip}&key=${process.env.REACT_APP_GOOGLE_GEOCODING}`
-//   );
-//   const coords = res.data.results[0].geometry.location;
-//   // dispatch({ type: D_GET_COORDS, payload: coords });
-
-//   return coords;
-// };
-//
 
 export const setCoordsAct = (coords) => (dispatch) => {
   dispatch({ type: D_SET_COORDS, payload: coords });
