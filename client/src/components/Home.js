54919b4d26ed9208dfc904a2c26b0d1f05a7d9cb
jhoonhan <jhoon.han@outlook.com>
2022-03-16 14:40:26 -0400

    background animation

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -26,35 +26,54 @@ const Home = ({ user }) => {
   };
   const render = () => {
     return (
-      <div className="landing__container">
+      <>
         <div className="landing__background">
-          <svg viewBox="0 0 800 370" className="ui-icon">
-            <use href={`${landingBackground}#sun--6`} className="ui-icon"></use>
+          <svg viewBox="0 0 800 370" className="background--1">
+            <use href={`${landingBackground}#sun--6`}></use>
+            <use href={`${landingBackground}#sun--5`}></use>
+            <use href={`${landingBackground}#sun--4`}></use>
+            <use href={`${landingBackground}#sun--3`}></use>
+            <use href={`${landingBackground}#sun--2`}></use>
+            <use href={`${landingBackground}#sun--1`}></use>
+            <use href={`${landingBackground}#mountain--2`}></use>
+            <use href={`${landingBackground}#mountain--1`}></use>
+            <use href={`${landingBackground}#cloud--4`}></use>
+            <use href={`${landingBackground}#cloud--3`}></use>
+            <use href={`${landingBackground}#cloud--2`}></use>
+            <use href={`${landingBackground}#cloud--1`}></use>
+          </svg>
+
+          <svg viewBox="0 0 800 370" className="background--2">
+            <use href={`${landingBackground}#lake`}></use>
+            <use href={`${landingBackground}#tree--2`}></use>
+            <use href={`${landingBackground}#tree--1`}></use>
           </svg>
         </div>
-        <div className="landing__welcome">
-          <h2>Good morning</h2>
-          <h1>{user.currentUser.firstName}</h1>
+        <div className="landing__container">
+          <div className="landing__welcome">
+            <h2>Good morning</h2>
+            <h1>{user.currentUser.firstName}</h1>
+          </div>
+          <div className="landing__status">
+            <h4 style={{ textTransform: "uppercase" }}>
+              your most recent order:
+            </h4>
+            <StatusBar order={user.currentUser.orders.slice(-1)[0]} />
+          </div>
+          <div className="landing__container__buttons">
+            <Link className="button--l" to={`/pickup`}>
+              Request new order
+            </Link>
+          </div>
+          <div className="landing__orders">
+            <label>your recent orders</label>
+            <div className="order__list">{renderList()}</div>
+            <Link className="button--l" to={`/order`}>
+              Your orders
+            </Link>
+          </div>
         </div>
-        <div className="landing__status">
-          <h4 style={{ textTransform: "uppercase" }}>
-            your most recent order:
-          </h4>
-          <StatusBar order={user.currentUser.orders.slice(-1)[0]} />
-        </div>
-        <div className="landing__container__buttons">
-          <Link className="button--l" to={`/pickup`}>
-            Request new order
-          </Link>
-        </div>
-        <div className="landing__orders">
-          <label>your recent orders</label>
-          <div className="order__list">{renderList()}</div>
-          <Link className="button--l" to={`/order`}>
-            Your orders
-          </Link>
-        </div>
-      </div>
+      </>
     );
   };
   return render();
