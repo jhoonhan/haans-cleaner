2564e06977c0ff0d936bee401d9033776594cbd8
jhoonhan <jhoon.han@outlook.com>
2022-03-22 20:09:43 -0400

    animation working

--- client/src/components/signIn/SignInGoogle.js
+++ client/src/components/signIn/SignInGoogle.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { motion } from "framer-motion";
-import sunBackground from "../../image/sunBackground.svg";
 import signupBackground from "../../image/signupBackground.svg";
 
 import GoogleButton from "../../apis/GoogleButton";
@@ -10,52 +9,67 @@ const SignInGoogle = ({ auth }) => {
   const showClass = auth.isSignedIn ? "hidden" : "";
 
   return (
-    <div className={`signIn__init__container ${showClass}`}>
-      <div className="signIn__background">
-        <motion.div
-          initial={{ opacity: 0, y: "0vh" }}
-          animate={{ opacity: 1, y: "-25vh" }}
-          transition={{ duration: 0.5, delay: 0.3 }}
-        >
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ type: "spring", duration: 0.5 }}
+      style={{ width: "100%" }}
+    >
+      <div className={`signIn__init__container ${showClass}`}>
+        <div className="signIn__background">
+          <motion.div
+            initial={{ opacity: 0, y: "0vh" }}
+            animate={{ opacity: 1, y: "-25vh" }}
+            transition={{ duration: 0.5, delay: 0 }}
+            className="singIn__background--sun"
+          >
+            <svg viewBox="0 0 800 1600" className="signIn__background--1">
+              <use href={`${signupBackground}#sky`}></use>
+            </svg>
+          </motion.div>
+
           <svg viewBox="0 0 800 1600" className="signIn__background--1">
-            <use href={`${signupBackground}#sky`}></use>
+            <use href={`${signupBackground}#background`}></use>
           </svg>
-        </motion.div>
-        <svg viewBox="0 0 800 1600" className="signIn__background--1">
-          <use href={`${signupBackground}#background`}></use>
-          <use href={`${signupBackground}#van`}></use>
-        </svg>
 
-        <svg viewBox="0 0 800 640" className="signIn__background--2">
-          <use href={`${signupBackground}#phone`}></use>
-          <use href={`${signupBackground}#clothes`}></use>
-        </svg>
+          <svg
+            viewBox="0 0 800 1600"
+            className="signIn__background--1 singIn__background--van"
+          >
+            <use href={`${signupBackground}#van`}></use>
+          </svg>
 
-        <motion.div
-          initial={{ x: "-50vw", y: "50vh" }}
-          animate={{ x: "0vw", y: "50vh" }}
-          transition={{ duration: 0.2, delay: 0.4 }}
-        >
           <svg viewBox="0 0 800 640" className="signIn__background--2">
-            <use href={`${signupBackground}#figure`}></use>
+            <use href={`${signupBackground}#phone`}></use>
+            <use href={`${signupBackground}#clothes`}></use>
           </svg>
-        </motion.div>
+
+          <motion.div
+            initial={{ x: "-50vw", y: "50vh" }}
+            animate={{ x: "0vw", y: "50vh" }}
+            transition={{ duration: 0.2, delay: 0.4 }}
+          >
+            <svg viewBox="0 0 800 640" className="signIn__background--2">
+              <use href={`${signupBackground}#figure`}></use>
+            </svg>
+          </motion.div>
+        </div>
+        <div className="signIn__initial">
+          <div className="signIn__row">
+            <h2>
+              Easy & Modern <br />
+              Dry Cleaning
+            </h2>
+          </div>
+          <div className="signIn__row">
+            <p>Join now and your first order is on us!</p>
+          </div>
+          <div className="signIn__row" style={{ marginTop: "3rem" }}>
+            <GoogleButton />
+          </div>
+        </div>
       </div>
-      <div className="signIn__initial">
-        <div className="signIn__row">
-          <h2>
-            Easy & Modern <br />
-            Dry Cleaning
-          </h2>
-        </div>
-        <div className="signIn__row">
-          <p>Join now and your first order is on us!</p>
-        </div>
-        <div className="signIn__row" style={{ marginTop: "3rem" }}>
-          <GoogleButton />
-        </div>
-      </div>
-    </div>
+    </motion.div>
   );
 };
 
